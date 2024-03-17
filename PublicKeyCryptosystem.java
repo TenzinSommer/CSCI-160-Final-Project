bd8a676e4f748e950e6ef6eca0de2411db637e61
Mia Kelley-Lanser <89313852+MiaKL@users.noreply.github.com>
2024-03-17 12:10:17 -0400

    Add files via upload

---
+++ PublicKeyCryptosystem.java
@@ -0,0 +1,56 @@
+import java.math.BigInteger;
+
+public abstract class PublicKeyCryptosystem {
+    protected BigInteger modulus = new BigInteger("1");
+    protected BigInteger privateKey = new BigInteger("1");
+    protected BigInteger publicKey = new BigInteger("1");
+
+    protected PublicKeyCryptosystem() {
+        setModulus();
+        setPrivateKey(2);
+        setPublicKey();
+    }
+    protected PublicKeyCryptosystem(BigInteger privateKey) {
+        setModulus();
+        setPrivateKey(privateKey);
+        setPublicKey();
+    }
+    protected PublicKeyCryptosystem(int privateKey) {
+        setModulus();
+        setPrivateKey(privateKey);
+        setPublicKey();
+    }
+    protected PublicKeyCryptosystem(BigInteger modulus, BigInteger privateKey) {
+        setModulus(modulus);
+        setPrivateKey(privateKey);
+        setPublicKey();
+    }
+    protected PublicKeyCryptosystem(int modulus, int privateKey) {
+        setModulus(modulus);
+        setPrivateKey(privateKey);
+        setPublicKey();
+    }
+
+    public abstract void setModulus();
+    public void setModulus(BigInteger modulus) {
+        this.modulus = modulus;
+        setPublicKey();
+    }
+    public void setModulus(int modulus) {
+        this.modulus = BigInteger.valueOf(modulus);
+        setPublicKey();
+    }
+    protected abstract void setPrivateKey(BigInteger privateKey);
+    protected abstract void setPrivateKey(int privateKey);
+    public abstract void setPublicKey();
+
+    public BigInteger getModulus() {
+        return modulus;
+    }
+    public BigInteger getPrivateKey() {
+        return privateKey;
+    }
+    public BigInteger getPublicKey() {
+        return publicKey;
+    }
+}
